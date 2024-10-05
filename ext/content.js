31187e80771d550dcaabd00110982213300f20a8
elgordo <water7baby@hotmail.com>
2024-10-05 11:09:06 +0800

    fix key interuption by introduce observer

--- ext/content.js
+++ ext/content.js
@@ -1,56 +1,66 @@
+
+let isTyping = false;
+const observer = new MutationObserver((mutations) => {
+    if (mutations.length > 0) {
+        let searchInput = document.querySelector('.nav-search-input');
+        if (searchInput) {
+            console.log(searchInput);
+            searchInput.addEventListener('focus', () => {
+                isTyping = true;
+            });
+            searchInput.addEventListener('blur', () => {
+                isTyping = false;
+            });
+            observer.disconnect(); // 找到后停止观察
+        }
+    }
+});
+observer.observe(document.body, { childList: true, subtree: true });
+
+const data = {
+    'a': '.bpx-player-dm-input',
+    'b': 'video',
+    'g': '.bpx-player-ctrl-web',
+    'r': '.primary-btn.roll-btn',
+    't': '.bpx-player-ctrl-wide',
+    'v': '.mini-player-window.fixed-sidenav-storage-item'
+};
+
 document.addEventListener('keydown', (event) => {
+    // 如果正在输入，则不处理按键事件
+    if (isTyping) {
+        return;
+    }
     // 确保只处理单个按键按下，并且不处理修饰键
     if (event.ctrlKey || event.altKey || event.shiftKey || event.metaKey) {
         return;
     }
 
     try {
-        // 获取当前页面的主机名和播放器
-        const hostname = window.location.hostname;
-        const player = document.querySelector('.bpx-player-control-wrap'); // Bilibili 播放器
+        let key = event.key.toLowerCase();
+        let element = data[key] ? document.querySelector(data[key]) : null;
 
-        // 按下 'R' 刷新页面，仅在主页有效
-        if ((event.key.toLowerCase() === 'r') && hostname === 'www.bilibili.com') {
-            const refreshButton = document.querySelector('.primary-btn.roll-btn');
-            if (refreshButton) {
-                refreshButton.click(); // 刷新页面
-            }
-            return; // 添加此行以避免执行后续逻辑
-        }
-
-        // 如果不在播放器页面，返回
-        if (!player) return;
-
-        // 按下 'T' 切换宽屏模式
-        if (event.key.toLowerCase() === 't') {
-            const widescreenButton = document.querySelector('.bpx-player-ctrl-wide');
-            widescreenButton?.click(); // 使用可选链操作符
-        }
-
-        // 按下 'G' 切换网页全屏模式
-        if (event.key.toLowerCase() === 'g') {
-            const webFullscreenButton = document.querySelector('.bpx-player-ctrl-web');
-            webFullscreenButton?.click(); // 使用可选链操作符
-        }
-
-        // 按下 'B' 重新开始播放视频
-        if (event.key.toLowerCase() === 'b') {
-            const video = document.querySelector('video');
-            if (video) {
-                video.currentTime = 0; // 将播放进度设置为 0
-                video.play(); // 开始播放
-            }
-        }
-
-        // 按下 'A' 聚焦文本框
-        if (event.key.toLowerCase() === 'a') {
-            event.preventDefault();
-            const textfield = document.querySelector('.bpx-player-dm-input');
-            if (textfield) {
-                textfield.focus();
+        if (element) {
+            switch (key) {
+                case 'r':
+                case 't':
+                case 'g':
+                case 'v':
+                    element.click();
+                    break;
+                case 'a':
+                    event.preventDefault(); // 阻止默认行为，防止输入
+                    element.focus();
+                    //setTimeout(() => {element.textContent = '';}, 0); // 清空输入框内容
+                    break;
+                case 'b':
+                    element.currentTime = 0;
+                    element.play();
+                    break;
             }
         }
     } catch (error) {
         console.error('bilibili快捷键: 处理按键事件时发生错误:', error); // 输出错误信息到控制台
     }
 });
+
--- readme.md
+++ readme.md
@@ -2,18 +2,20 @@
 ## 简介
 这是一个为Bilibili网站定制的用户脚本，它为用户提供了简单的快捷键功能，以便更方便地使用Bilibili平台。
 ## 功能
+- 按 `A` 聚焦弹幕输入框
+- 按 `B` 重新开始播放当前视频，按住阻止播放
+- 按 `G` 切换网页全屏模式
 - 按 `R` 主页刷新推荐视频
 - 按 `T` 切换宽屏模式
-- 按 `G` 切换网页全屏模式
-- 按 `B` 重新开始播放当前视频，按住阻止播放
-- 按 `A` 聚焦弹幕输入框
+- 按 `V` 隐藏/显示小窗口
 
 # bilibili shortcut UserScript
 ## Introduction
 This is a user script customized for Bilibili, which provides users with simple shortcut functionalities for easier use of the Bilibili platform.
 ## Features
+- Press `A` to focus on the bullet chat input box
+- Press `B` to replay the current video from the start, holding down prevent play
+- Press `G` to toggle web full screen mode
 - Press `R` to refresh the recommended videos on main page
 - Press `T` to toggle wide screen mode
-- Press `G` to toggle web full screen mode
-- Press `B` to replay the current video from the start, holding down prevent play
-- Press `A` to focus on the bullet chat input box
+- Press `V` to toggle mini window player display
